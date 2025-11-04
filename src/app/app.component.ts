2ec370db0e1157f9ed26f4b16fa094c332da3300
Alexander Putilin <alexander.putilin@yahoo.de>
2025-11-04 21:02:08 +0100

    feat: hero section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HerosectionComponent } from './herosection/herosection.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    NavbarComponent,
+    HerosectionComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'portfolio';
+  title = 'Portfolio';
 }
---
+++ src/app/herosection/herosection.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { SocialsmenuComponent } from '../socialsmenu/socialsmenu.component';
+import { NavarrowComponent } from '../navarrow/navarrow.component';
+
+@Component({
+  selector: 'app-herosection',
+  imports: [SocialsmenuComponent, NavarrowComponent],
+  templateUrl: './herosection.component.html',
+  styleUrl: './herosection.component.scss'
+})
+export class HerosectionComponent {
+
+}

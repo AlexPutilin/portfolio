9a0f123efd9f133278621cb7afad24443ec5670b
Alexander Putilin <alexander.putilin@yahoo.de>
2025-11-06 19:40:04 +0100

    feat: modular button component

---
+++ src/app/ui/buttons/button.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss'
+})
+export class ButtonComponent {
+  @Input() type: 'button' | 'submit' | 'reset' = 'button';
+  @Input() variant: 'primary' | 'secondary' = 'primary';
+  @Input() disabled: boolean = false;
+}

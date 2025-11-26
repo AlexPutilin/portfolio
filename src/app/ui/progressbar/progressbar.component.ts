ec42f347c06f40cd4180510ed656b20e2f539013
Alexander Putilin <alexander.putilin@yahoo.de>
2025-11-26 19:11:36 +0100

    fix: removed progressbar.

---
+++ src/app/ui/progressbar/progressbar.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-progressbar',
+  imports: [],
+  templateUrl: './progressbar.component.html',
+  styleUrl: './progressbar.component.scss'
+})
+export class ProgressbarComponent {
+  @Input() progress = 0;
+}

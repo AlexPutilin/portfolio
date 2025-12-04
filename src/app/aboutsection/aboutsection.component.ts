9fa803f09f86733fd945c6580f83f5cf1be947c9
Alexander Putilin <alexander.putilin@yahoo.de>
2025-12-04 19:43:29 +0100

    refactor: rebuild about-me section

--- src/app/aboutsection/aboutsection.component.ts
+++ src/app/aboutsection/aboutsection.component.ts
@@ -1,13 +1,35 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { ButtonComponent } from '../ui/buttons/button.component';
-import { NavarrowComponent } from '../ui/navarrow/navarrow.component';
+import { afterNextRender, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-aboutsection',
-  imports: [ButtonComponent, NavarrowComponent],
+  imports: [],
   templateUrl: './aboutsection.component.html',
   styleUrl: './aboutsection.component.scss'
 })
 export class AboutsectionComponent {
-  @Output() nextSection = new EventEmitter()
+  constructor() {
+    afterNextRender(() => {
+      this.initObserver();
+    });
+  }
+
+  @ViewChild("profile", { read: ElementRef }) profileDiv!: ElementRef;
+  @ViewChild("aboutMe", { read: ElementRef }) aboutMeDiv!: ElementRef;
+
+  observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        (entry.target as HTMLElement).classList.add('in-view');
+      }
+    });
+  }, { threshold: 0.4 });
+
+  private initObserver() {
+    if (this.aboutMeDiv) {
+      this.observer.observe(this.aboutMeDiv.nativeElement);
+    }
+    if (this.profileDiv) {
+      this.observer.observe(this.profileDiv.nativeElement);
+    }
+  }
 }

29250d57782d44d0217e9899a603d3a7c2e22092
Alexander Putilin <alexander.putilin@yahoo.de>
2025-12-05 18:21:27 +0100

    refactor: rebuild skills section

--- src/app/skillssection/skillssection.component.ts
+++ src/app/skillssection/skillssection.component.ts
@@ -1,10 +1,11 @@
 import { afterNextRender, Component, ElementRef, EventEmitter, Output, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { SkillboxComponent } from '../skillbox/skillbox.component';
 import { NavarrowComponent } from '../ui/navarrow/navarrow.component';
+import { ButtonComponent } from '../ui/buttons/button.component';
 
 @Component({
   selector: 'app-skillssection',
-  imports: [SkillboxComponent, NavarrowComponent],
+  imports: [SkillboxComponent, ButtonComponent],
   templateUrl: './skillssection.component.html',
   styleUrl: './skillssection.component.scss'
 })

a2d8e7b731e9464e12974304d899347d1ac7e7a9
Alexander Putilin <alexander.putilin@yahoo.de>
2025-12-11 20:05:38 +0100

    style: optimize responsiveness

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -3,10 +3,11 @@ import { HerosectionComponent } from '../herosection/herosection.component';
 import { AboutsectionComponent } from '../aboutsection/aboutsection.component';
 import { HeaderComponent } from '../header/header.component';
 import { SkillssectionComponent } from '../skillssection/skillssection.component';
+import { ProjectssectionComponent } from '../projectssection/projectssection.component';
 
 @Component({
   selector: 'app-mainpage',
-  imports: [HeaderComponent, HerosectionComponent, AboutsectionComponent, SkillssectionComponent],
+  imports: [HeaderComponent, HerosectionComponent, AboutsectionComponent, SkillssectionComponent, ProjectssectionComponent],
   templateUrl: './mainpage.component.html',
   styleUrl: './mainpage.component.scss',
   standalone: true

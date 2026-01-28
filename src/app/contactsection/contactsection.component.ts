ce89a07088ac3208b25498d5011d09ac97d196cc
Alexander Putilin <alexander.putilin@yahoo.de>
2026-01-28 09:03:59 +0100

    feat: contact form

--- src/app/contactsection/contactsection.component.ts
+++ src/app/contactsection/contactsection.component.ts
@@ -1,14 +1,59 @@
-import { Component } from '@angular/core';
-import { InputsComponent } from '../ui/inputs/textinput/textinput.component';
+import { Component, inject } from '@angular/core';
 import { ButtonComponent } from '../ui/buttons/button/button.component';
-import { CheckboxComponent } from '../ui/inputs/checkbox/checkbox.component';
+import { FormsModule, NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contactsection',
-  imports: [InputsComponent, ButtonComponent, CheckboxComponent],
+  imports: [ButtonComponent, FormsModule],
   templateUrl: './contactsection.component.html',
-  styleUrl: './contactsection.component.scss'
+  styleUrl: './contactsection.component.scss',
+  standalone: true
 })
 export class ContactsectionComponent {
-  formIsValid: boolean = false;
+  privacyAccepted: boolean = false;
+  hasSubmitted: boolean = false;
+  
+  mailTest = true;
+
+  http = inject(HttpClient);
+
+  contactData = {
+    name: "",
+    email: "",
+    message: ""
+  }
+
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    this.hasSubmitted = true;
+    if (ngForm.valid && ngForm.submitted && !this.mailTest) {
+      this.http
+        .post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.valid && this.mailTest) {
+      ngForm.resetForm();
+      console.log(`Email submitted: Contact, ${this.contactData}`);
+    }
+  }
+
+
 }
